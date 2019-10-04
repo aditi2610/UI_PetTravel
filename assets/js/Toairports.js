6292d074760365e7cb315e777bfd01c20d5bbebf
Aditi Mangal <aditimangal@adityag1-2.local>
2019-10-03 18:09:27 -0700

    Flyers added

--- assets/js/Toairports.js
+++ assets/js/Toairports.js
@@ -8,7 +8,7 @@ function getToAirports() {
     request.onload = function () {
         if (request.status === 200) {
             const data = JSON.parse(request.responseText);
-        
+            localStorage.setItem("airports", JSON.stringify(data));
             var sel = document.getElementById('SelectAirTo');
             
             for (var key in data) {
@@ -32,4 +32,28 @@ function getToAirports() {
     };
 
     request.send()
+}
+
+function nextPage() {
+    var toAirport = document.getElementById("SelectAirTo").value;
+    var fromAirport = document.getElementById("SelectAir").value;
+    var sourceDate = document.getElementById("sourceDate").value;
+
+    var dest = "";
+    var source = "";
+    var source_date = "";
+
+    airports = JSON.parse(localStorage.getItem("airports"))
+    for (var key in airports) {
+        if (airports[key] == fromAirport) {
+            dest = key;
+        }
+
+        if (airports[key] == toAirport) {
+            source = key;
+        }
+    }
+
+    url = "../../pickflyer.html?" + "source=" + source + "&dest=" + dest + "&source_date=" + sourceDate;
+    window.location = url;
 }
---
+++ assets/js/flyers.js
@@ -0,0 +1,44 @@
+function fillFlyers() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var source = urlParams.get('source')
+    var dest = urlParams.get('dest')
+    var source_date = urlParams.get('source_date')
+
+    const url = `http://localhost:8000/united/plan?source=${source}&dest=${dest}&source_date=${source_date}`;
+    const request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function () {
+        if (request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            localStorage.setItem("plan", JSON.stringify(data));
+            console.log(data["onwards"]);
+            var onwards = data["onwards"]
+            var count = 1;
+            for (var key in onwards) {
+                if (count > 3) {
+                    break;
+                }
+
+                console.log(onwards[key])
+                var flyer_data = onwards[key]
+
+                var sel = document.getElementById(`flyer${count}_data`);
+                sel.innerHTML = `${flyer_data["Departure"]["Time"]} | ${flyer_data["Departure"]["AirportCode"]} -&gt; ${flyer_data["Arrival"]["AirportCode"]}<br>Rating Stars: 4/5
+                <br>Testimonials: Super Awesome!<br>`
+
+                var sel = document.getElementById(`flyer${count}_name`);
+                sel.innerHTML = `${flyer_data["kinship_name"]} | ${flyer_data["kinship_id"]}`
+
+                count++;
+            }
+        } else {
+            console.log("I met an Error");
+        }
+    }
+
+    request.onerror = function () {
+        console.error('An error occurred fetching the JSON from ' + url);
+    };
+    request.send()
+}
